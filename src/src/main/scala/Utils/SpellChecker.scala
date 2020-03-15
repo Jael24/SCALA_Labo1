84a68db801a701357e69aaa391af916b93a0cc2a
Laykel <layke.luc@gmail.com>
2020-03-15 15:59:56 +0100

    Add more comment, reformat

--- src/src/main/scala/Utils/SpellChecker.scala
+++ src/src/main/scala/Utils/SpellChecker.scala
@@ -12,24 +12,21 @@ object SpellChecker {
    * @return an integer value, which indicates the Levenshtein distance between "s1" and "s2"
    */
   def stringDistance(s1: String, s2: String): Int = {
-    listDistance(s1.toList, s2.toList)
-  }
-
-  /**
-   * Helper that actually calculates the Levenshtein distance between two words.
-   *
-   * @param l1 the first word
-   * @param l2 the second word
-   * @return an integer value, which indicates the Levenshtein distance between "s1" and "s2"
-   */
-  def listDistance(l1: List[Char], l2: List[Char]): Int = (l1, l2) match {
-    case (Nil, l2) => l2.length
-    case (l1, Nil) => l1.length
-    case (x :: xs, y :: ys) => if (x == y) {
-      listDistance(xs, ys)
-    } else {
-      1 + min(listDistance(l1, ys), min(listDistance(xs, l2), listDistance(xs, ys)))
+    // Define a function for the lists
+    def listDistance(l1: List[Char], l2: List[Char]): Int = (l1, l2) match {
+      // If one of the lists is empty, return the length of the other
+      case (Nil, l2) => l2.length
+      case (l1, Nil) => l1.length
+      case (x :: xs, y :: ys) => if (x == y) {
+        // If the elements are identical, don't add to the distance
+        listDistance(xs, ys)
+      } else {
+        // If they aren't, add 1 to the distance and take the shortest path to the word
+        1 + min(listDistance(l1, ys), min(listDistance(xs, l2), listDistance(xs, ys)))
+      }
     }
+
+    listDistance(s1.toList, s2.toList)
   }
 
   /**
@@ -39,14 +36,14 @@ object SpellChecker {
    * @param misspelledWord the misspelled word to correct
    * @return the closest word from "misspelledWord"
    */
-  def getClosestWordInDictionary(misspelledWord: String): String = {
-    var score = ("", Int.MaxValue)
-
-    // Return the word if it's a number or a username
+  def getClosestWordInDictionary(misspelledWord: String): String =
     if (misspelledWord.forall(_.isDigit) || misspelledWord.startsWith("_")) {
-      return misspelledWord
+      // Return the word if it's a number or a username
+      misspelledWord
     } else {
       // Else, get the smallest distance between the given word and a word in the dictionary
+      var score = ("", Int.MaxValue)
+
       for (key <- dictionary.keys) {
         val temp = stringDistance(misspelledWord, key)
 
@@ -54,8 +51,7 @@ object SpellChecker {
           score = (key, temp)
         }
       }
-    }
 
-    dictionary(score._1)
-  }
+      dictionary(score._1)
+    }
 }
